d597d6cef83d6e2ef804b76941dc3c36b29fc993
Joost de Valk <joost@altha.nl>
2024-02-22 09:28:59 +0100

    Initial commit

---
+++ eco-friendly-robots-txt.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Eco-Friendly Robots.txt
+ * Plugin URI: https://joost.blog/plugins/eco-friendly-robots-txt/
+ * Description: Optimizes your site's robots.txt to reduce server load and CO2 footprint by blocking unnecessary crawlers while allowing major search engines and specific tools.
+ * Version: 0.5
+ * Author: Joost de Valk
+ * Author URI: https://joost.blog/
+ * License: GPL v2 or later
+ *
+ * @package Emilia\EcoFriendlyRobotsTxt
+ */
+
+namespace Emilia\EcoFriendlyRobotsTxt;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Direct script access disallowed.' );
+}
+
+require_once plugin_dir_path( __FILE__ ) . 'src/class-plugin.php';
+
+/**
+ * Initialize the plugin.
+ *
+ * @return void
+ */
+function ecofriendly_robotstxt_init() {
+	$eco_friendly_robots = new Plugin();
+	$eco_friendly_robots->init();
+}
+
+add_action( 'plugins_loaded', 'ecofriendly_robotstxt_init' );
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Eco-Friendly Robots.txt ===
+Contributors: joostdevalk
+Tags: robots.txt, crawler, crawlers, seo
+Requires at least: 6.2
+Tested up to: 6.4
+Requires PHP: 7.4
+License: GPL3+
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+Optimizes your site's robots.txt to reduce server load and CO2 footprint by blocking unnecessary crawlers while allowing major search engines and specific tools.
+
+== Description ==
+
+Optimizes your site's robots.txt to reduce server load and CO2 footprint by blocking unnecessary crawlers while allowing major search engines and specific tools.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release.
